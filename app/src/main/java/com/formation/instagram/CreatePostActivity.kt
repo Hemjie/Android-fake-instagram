3fd7d0f8ca243f7e9b382d2382881624dfcd1032
Hemjie <marjolaine.vilain@gmail.com>
2021-04-09 10:05:16 +0200

    instanciation variables create_post

--- app/src/main/java/com/formation/instagram/CreatePostActivity.kt
+++ app/src/main/java/com/formation/instagram/CreatePostActivity.kt
@@ -1,15 +1,29 @@
 package com.formation.instagram
 
+import android.content.Context
 import android.content.Intent
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ImageView
+import android.widget.TextView
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class CreatePostActivity : AppCompatActivity(), View.OnClickListener {
 
+    lateinit var sp: SharedPreferences
     lateinit var bottomNavigationView: BottomNavigationView
+
+    lateinit var nickname: TextView
+    lateinit var description: EditText
+    lateinit var descriptionCount: TextView
+    lateinit var btnOpenGallery: Button
+    lateinit var photoBlur: ImageView
+    lateinit var photo: ImageView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,6 +31,18 @@ class CreatePostActivity : AppCompatActivity(), View.OnClickListener {
 
         bottomNavigationViewAction()
         findViewById<FloatingActionButton>(R.id.fab).setOnClickListener(this)
+
+        sp = getSharedPreferences("User", Context.MODE_PRIVATE)
+
+        nickname = findViewById(R.id.nickname)
+        description = findViewById(R.id.description)
+        descriptionCount = findViewById(R.id.description_count)
+        btnOpenGallery = findViewById(R.id.btn_open_gallery)
+        photoBlur = findViewById(R.id.photo_blur)
+        photo = findViewById(R.id.photo)
+
+
+
     }
 
     override fun onClick(v: View?) {
