e86b296af11a41b1a014a0e12e45d141ffc0542b
Hemjie <marjolaine.vilain@gmail.com>
2021-04-09 09:10:23 +0200

    configuration page create_post

--- app/src/main/java/com/formation/instagram/CreatePostActivity.kt
+++ app/src/main/java/com/formation/instagram/CreatePostActivity.kt
@@ -1,11 +1,45 @@
 package com.formation.instagram
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
-class CreatePostActivity : AppCompatActivity() {
+class CreatePostActivity : AppCompatActivity(), View.OnClickListener {
+
+    lateinit var bottomNavigationView: BottomNavigationView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_create_post)
+
+        bottomNavigationViewAction()
+        findViewById<FloatingActionButton>(R.id.fab).setOnClickListener(this)
+    }
+
+    override fun onClick(v: View?) {
+    }
+
+    private fun bottomNavigationViewAction(){
+        bottomNavigationView = findViewById(R.id.bottom_navigation_view)
+        bottomNavigationView
+            .menu.findItem(R.id.action_home)
+            .isChecked = true
+
+        bottomNavigationView.setOnNavigationItemSelectedListener { item ->
+            when(item.itemId){
+                R.id.action_home -> {
+                    true
+                }
+                R.id.action_user -> {
+                    val intent = Intent(this, UserActivity::class.java)
+                    startActivity(intent)
+                    true
+                }
+                else -> false
+            }
+        }
     }
 }
